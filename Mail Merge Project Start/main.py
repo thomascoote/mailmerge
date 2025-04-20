e2d944ffa6dfcaebc60a9a8597c7213371cde8fa
Tom <thomascoote89@gmail.com>
2025-04-21 00:15:01 +0100

    1

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -2,7 +2,8 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-    
+from os import write
+
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
@@ -13,11 +14,16 @@
 #TODO-4: Repeat until last index
 #TODO-5: Save each file in the Output Directory
 
-#1
-#Declare Invitee name list
-inv_list = []
-with open("Input/Names/invited_names.txt") as invites:
-    #Read each line in file
-    for line in invites:
-        str.replace("\n","")
-        inv_list.append(line)
+#File of names
+with open("Input/Names/invited_names.txt","r") as inv_text:
+    invite = inv_text.read()
+    cleaned = invite.strip()
+    inv_list = cleaned.splitlines()
+
+with open("Input/Letters/starting_letter.txt","r") as starting_letter:
+    letter = starting_letter.read()
+    iter_count = len(inv_list)
+    for i in range (0, iter_count-1):
+        new_letter = letter.replace("[name]",inv_list[i])
+
+        with open()
